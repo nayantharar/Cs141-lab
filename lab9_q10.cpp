8a64dc2b86a705e2f7f470e109c17bbcd1077a1d
nayantharar <42238330+nayantharar@users.noreply.github.com>
2018-11-15 11:54:36 +0530

    Update lab9_q10.cpp

--- lab9_q10.cpp
+++ lab9_q10.cpp
@@ -1,17 +1,21 @@
+//include library
 #include <iostream>
 #include<cstring>
 using namespace std;
+
 // the function pointer in parameter
 void revtString(char* p)
 {
     int end;
     int count=0;
     //using conditions to reverse the string
+    
     for(int i=0;(*(p+i))!='\0';i++)
     {
         count++;
     }
     end=count-1;
+    
     for(int i=end;i>=0;i--)
     {
         cout<<*(p+i);
@@ -26,6 +30,7 @@ int main()
     char ar[size];
     cout<<"Enter input to array"<<endl;
         cin>>ar;
+    
     //calling the function and use of pointer.
     char* p=&ar[0];
     cout<<"reverse"<<endl;
