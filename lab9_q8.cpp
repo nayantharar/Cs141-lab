ca7598c644887ba6fa73aa20c9301b9171e35514
nayantharar <42238330+nayantharar@users.noreply.github.com>
2018-11-15 11:51:29 +0530

    Update lab9_q8.cpp

--- lab9_q8.cpp
+++ lab9_q8.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
-//using the function
+
+//use the function
 int myStrLen(char* p)
+    
 {
     int count=0;
     for(int i=0;*(p+i)!='\0';i++)
@@ -10,12 +12,15 @@ int myStrLen(char* p)
         count++;
     }
     return count;
+    
 }
 int main()
 {
-    char ar[100];
+    char ar[10];
     cout<<"Enter the string:"<<ar;
-    cin.getline(ar,100);//getting a string line
+    cin.getline(ar,10);
+    
+    //getting a string line
     char* p=&ar[0];
     cout<<"Length of the string:"<<myStrLen(p);
     return 0;
