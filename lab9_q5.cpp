3e4ca8f0c2c46ca896d3316cb691b398bf7427e0
nayantharar <42238330+nayantharar@users.noreply.github.com>
2018-11-15 11:53:13 +0530

    Update lab9_q5.cpp

--- lab9_q5.cpp
+++ lab9_q5.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
+//write main function
 int main()
 {
    char c[10] ="abcde";
    char* cptr;
    cptr=c;
-   //using the loops
-   for(int i=10;i>=0;i--)
+   
+   
+   //using loops
+   for(int i=10 ;i>=0 ;i--)
    {
        cout<<*(cptr+i);
    }
