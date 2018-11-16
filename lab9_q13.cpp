eab45ded59aa7a56972f4989b7e1e89f0eadda77
nayantharar <42238330+nayantharar@users.noreply.github.com>
2018-11-16 08:16:01 +0530

    Update lab9_q13.cpp

--- lab9_q13.cpp
+++ lab9_q13.cpp
@@ -6,14 +6,20 @@ using namespace std;
 int main()
 {
 	int ar[10]={1,2,3,4,5,6,7,8,9,10};
+	
+	
 //using loop to print array
+	
 	int *p;
 	cout<<"Printing the array elements using normal index method"<<endl;
 	for(int i=0;i<10;i++)
 
 	cout<<" "<<ar[i];
         cout<<endl;
+	
 //using loop to print array
+	
+	
 	cout<<" Printing array elements using pointer method"<<endl;
 	p=ar;
 	for(int i=0;i<10;i++)
